149fa0d6b130e2a8c992add1cd328254b4901c02
rodrigjs <rodrigjs@mail.gvsu.edu>
2021-04-25 18:41:40 -0400

    notification files

---
+++ Internship Materials/Order_Notification/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+function App() {
+  return (
+    <div>
+      <link href="style.css" type="text/css" rel="stylesheet"></link>
+      <img src="https://i.pinimg.com/originals/d9/77/50/d97750dd50b718b86d335826aa55d2fc.jpg"/>
+        <h1>***** Order Completed! *****</h1>
+        <p>Thank you for shopping at Wookie Wear!</p>
+    </div>
+  );
+}
+
+export default App;
